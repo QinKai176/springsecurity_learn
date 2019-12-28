c746de1762d66d93b7ba19362a521a9393efdf9f
qinkai <cpubarry2015@163.com>
2019-12-28 22:19:16 +0800

    初始化spring security模块

---
+++ src/main/java/com/betterarrow/springsecurity_learn/SpringsecurityLearnApplication.java
@@ -0,0 +1,13 @@
+package com.betterarrow.springsecurity_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityLearnApplication.class, args);
+    }
+
+}
