f1d18ff0bedb318f50cc0d8dd0d2821ab12dc04a
qinkai <cpubarry2015@163.com>
2019-12-29 16:48:11 +0800

    添加基于内存的多模块支持

--- src/main/java/com/betterarrow/springsecurity_learn/config/WebSecurityConfig.java
+++ src/main/java/com/betterarrow/springsecurity_learn/config/WebSecurityConfig.java
@@ -1,8 +1,14 @@
 package com.betterarrow.springsecurity_learn.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 
 /**
@@ -13,10 +19,41 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 // 2: 加载了AuthenticationConfiguration, 配置了认证信息
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+
+        manager.createUser(User.withUsername("user1").password("123").roles("USER").build());
+        manager.createUser(User.withUsername("admin").password("123").roles("USER", "ADMIN").build());
+        return manager;
+    }
+
+    /**
+     * 关于ant模式解析：
+     * 1.*用来匹配0或者任意数量的字符
+     * 2.**用来匹配0或者更多的目录
+     * 3.？匹配任意单个字符
+     *
+     * @param http
+     * @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/myLogin.html")
-                .loginProcessingUrl("/login")
+        http.authorizeRequests().
+                antMatchers("/admin/api/***").hasRole("ADMIN")
+                .antMatchers("/user/api/**").hasRole("USER")
+                .antMatchers("/app/api/**").permitAll()
+                .anyRequest().authenticated().and().formLogin()
+//                .loginPage("/myLogin.html")
+//                .loginProcessingUrl("/login")
                 .permitAll().and().csrf().disable();
     }
 }
